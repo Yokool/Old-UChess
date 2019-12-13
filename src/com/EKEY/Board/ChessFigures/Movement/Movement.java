258318280f9243706712dd9689af124542a8337d
Yokool <letsgamecze@gmail.com>
2019-12-13 13:43:09 +0100

    comments

--- src/com/EKEY/Board/ChessFigures/Movement/Movement.java
+++ src/com/EKEY/Board/ChessFigures/Movement/Movement.java
@@ -9,6 +9,16 @@ import com.EKEY.Board.ChessFigures.Figure;
 import com.EKEY.Misc.Camera;
 import com.EKEY.Misc.DataShare;
 
+/**
+ * A class for moving a figure, each figure in the game can have a any amount of Movement objects
+ * tied to it.
+ * 
+ * Using composition and configuration, you can get any result you want.
+ * 
+ * These movement objects are best configured using the constructor.
+ * 
+ * Preconfigured objects can be find in the Prototype class.
+ */
 public abstract class Movement {
 	
 	protected Figure figure;
@@ -17,16 +27,30 @@ public abstract class Movement {
 	
 	protected BoardTile lastTile = null;
 	
-	// xCoords =   1, 10 - Go 1 step 10 tmies
-	// first param - step, second param is number of repeats
-	
+	/**
+	 * The constructor of the Movement class
+	 * @param figure The figure this movement object is tied to.
+	 */
 	public Movement(Figure figure) {
 		this.figure = figure;
 	}
 	
-	
+	/**
+	 * This method must be implemented in each subclass of Movement. This is the most important method of the movement
+	 * system as it determines which tiles will be rendered with the dot on them and which ones are walkable.
+	 */
 	protected abstract void recalc();
 	
+	/**
+	 * This method should not be changed unless fully needed to, it renders the transparent dots showing
+	 * where you are allowed to go.
+	 * 
+	 * The tiles that the dot shows upon is determined by the bufferList which is centered around the recalc method
+	 * which must be overriden in subclass of Movement.
+	 * 
+	 * @param g This method is called in the Figure class to this parameter is filled in. Thus you shouldn't call this
+	 * method yourself.
+	 */
 	public void renderOptions(Graphics g) {
 		
 		bufferList.clear();
@@ -41,9 +65,16 @@ public abstract class Movement {
 		
 	}
 	
+	/**
+	 * This method should not be called directly, it is used in the ClickableMouse class.
+	 * 
+	 * This method is called everytime a user clicks a BoardTile.
+	 * 
+	 * @param tile
+	 */
 	public void notified(BoardTile tile) {
 		
-		if(bufferList.contains(tile)) {
+		if(bufferList.contains(tile)) { // When the tile is inside the bufferList only then can the figure be moved onto the tile
 			moveFigure(tile);
 		}
 		
@@ -55,10 +86,7 @@ public abstract class Movement {
 			return;
 		}
 		
-		
 		lastTile = tile;
-		
-		
 		DataShare.BOARD.moveFigureToTile(figure, lastTile);
 		
 		
--- src/com/EKEY/Board/ChessFigures/Movement/StepMovement.java
+++ src/com/EKEY/Board/ChessFigures/Movement/StepMovement.java
@@ -4,6 +4,9 @@ import com.EKEY.Board.BoardTile;
 import com.EKEY.Board.ChessFigures.Figure;
 import com.EKEY.Misc.DataShare;
 
+/**
+ * One of the basic Movement classes. Best used in repeated patterns.
+ */
 public class StepMovement extends Movement{
 	
 	private int xStep = 0;
@@ -12,13 +15,52 @@ public class StepMovement extends Movement{
 	
 	private int repeat = 0;
 	
-	
+
+	// xCoords =   1, 10 - Go 1 step 10 tmies
+	// first param - step, second param is number of repeats
+	/**
+	 * The constructor lets you configure the object to suit your needs.
+	 * 
+	 * The constructor is best explained with an example.
+	 * E:
+	 * xStep = 0
+	 * yStep = -1
+	 * repeat = 2
+	 * 
+	 * This will let you walk two tiles in the front. The tiles are above the figure since we are walking from it's location.
+	 * If the yStep was -2, they would have a space between them so you could walk
+	 * 
+	 * 
+	 * 
+	 * <div>
+	 * o
+	 * <br>
+	 * x
+	 * <br>
+	 * o
+	 * <br>
+	 * x
+	 * <br>
+	 * f
+	 * <div>
+	 * 
+	 * <div>
+	 * f figure
+	 * <br>
+	 * x not walkable
+	 * <br>
+	 * o walkable
+	 * <div>
+	 * 
+	 * @param xStep The amount of tiles in the x axis to step.
+	 * @param yStep The amount of tiles in the y axis to step.
+	 * @param repeat The number of times to repeat the loop.
+	 */
 	public StepMovement(Figure figure,int xStep, int yStep, int repeat) {
 		super(figure);
 		this.xStep = xStep;
 		this.repeat = repeat;
 		this.yStep = yStep;
-		
 	}
 
 	@Override
