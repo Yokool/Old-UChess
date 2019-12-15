cbc103f706c6965de01d62ca1bd2f033551cbbe4
Yokool <letsgamecze@gmail.com>
2019-12-15 18:11:47 +0100

    doc part 1

--- src/com/EKEY/Board/ChessFigures/Movement/Flinging/Fling.java
+++ src/com/EKEY/Board/ChessFigures/Movement/Flinging/Fling.java
@@ -2,10 +2,27 @@ package com.EKEY.Board.ChessFigures.Movement.Flinging;
 
 import com.EKEY.Board.ChessFigures.Figure;
 
+/**
+ * An abstract superclass of all Fling related logic (Throwing the figures out of the playing field)
+ * 
+ * A Fling class is tied to the Movement object. The tied figure should be the same as the one in the Movement class.
+ * This is usually set when the Movement class's figure is set.
+ * 
+ * This class contains a single method that is called in the Movement class.
+ */
 public abstract class Fling {
-	
+	/**
+	 * The tied figure to the Fling class. (The Figure that's throwing out another figure. NOT THE TARGET)
+	 */
 	protected Figure figure = null;
 	
+	/**
+	 * The method that gets called through the movement class. Should be overriden to do custom logic.
+	 * 
+	 * You shouldn't call this method on your own.
+	 * 
+	 * @param The targeted figure, which should already be filled out by the Movement object.
+	 */
 	public abstract void flingOut(Figure targetF);
 	
 	public Figure getThisF() {
--- src/com/EKEY/Board/ChessFigures/Movement/Flinging/VanillaFling.java
+++ src/com/EKEY/Board/ChessFigures/Movement/Flinging/VanillaFling.java
@@ -4,6 +4,9 @@ import com.EKEY.Board.BoardTile;
 import com.EKEY.Board.ChessFigures.Figure;
 import com.EKEY.Misc.DataShare;
 
+/**
+ * The most basic of the Fling classes. Works like in the normal chess game which "instantly" kills the figure, throwing it out.
+ */
 public class VanillaFling extends Fling{
 
 	@Override
